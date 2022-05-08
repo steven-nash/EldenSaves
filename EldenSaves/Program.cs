73ae47b9946c5dde6613fb62460cd716a57e1cc4
Steven Nash <stevennash@trentu.ca>
2022-05-08 01:43:37 -0400

    Project creation

---
+++ EldenSaves/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EldenSaves
+{
+    static class Program
+    {
+        /// Steven Nash 2022
+        /// Elden Saves is a Windows application for creating and managing Elden Ring save copies.
+        
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
